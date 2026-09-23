5dcfe6f1a0f5e0706d0c0d71fa7210d9f530c1a4
ycanakyuz-gif <ycanakyuz@gmail.com>
2026-09-23 21:17:56 +0300

    Update common.c

--- src/common.c
+++ src/common.c
@@ -382,6 +382,7 @@ void register_progress(uint32_t tag, const char* label)
 
 void finalize_progress(uint32_t tag)
 {
+/* Initialize progress state before finalizing */
 	thread_once(&progress_info_once, _init_progress_info);
 	mutex_lock(&prog_mutex);
 	struct progress_info_entry* found = NULL;
